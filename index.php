cf79f0350a100a5d20012055d1a54e0e41957a31
Ibrahim Al-Ashqar <eng.ibrahim20191406@gmail.com>
2023-03-11 17:59:01 +0200

    Make a PHP Router

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
-require "functions.php";
+require 'functions.php';
 
-$heading = "Home";
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-require "views/index.view.php";
+$routes = [
+  '/' => 'controllers/index.php',
+  '/about' => 'controllers/about.php',
+  '/contact' => 'controllers/contact.php'
+];
+
+function routeToController($uri, $routes) {
+  if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+  } else {
+    abort();
+  }
+}
+
+function abort($code = 404){
+  http_response_code($code);
+
+  require "views/{$code}.php";
+
+  die();
+}
+
+routeToController($uri, $routes);
