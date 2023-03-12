e761f1345f93e22e23a6913fe7340ca8dbf74da3
Ibrahim Al-Ashqar <eng.ibrahim20191406@gmail.com>
2023-03-13 00:19:44 +0200

    PDO

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
 require 'functions.php';
+// require 'router.php';
 
-require 'router.php';
+
+// Connect to our MYSQL database  
+$dsn = "mysql:host=localhost;port:3306;dname=demo;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['title'] , "</li>"; 
+}
