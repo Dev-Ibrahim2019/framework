d6c807d6e870889a7ea9386f57e7db5f27e2e93e
Ibrahim Al-Ashqar <eng.ibrahim20191406@gmail.com>
2023-03-13 16:32:08 +0200

    Environments and Configration Flexibility

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 
 require 'functions.php';
-// require 'router.php';
+require 'Database.php';
 
+$config = require 'config.php';
 
-// Connect to our MYSQL database  
-$dsn = "mysql:host=localhost;port:3306;dname=demo;charset=utf8mb4";
-$pdo = new PDO($dsn, 'root');
+$db = new Database($config['database']);
 
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
+$posts = $db->query("SELECT * FROM demo.posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
   echo "<li>" . $post['title'] , "</li>"; 
 }
+  
